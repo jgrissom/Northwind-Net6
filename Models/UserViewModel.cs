0f7282c0f96365ef3ffa77e0973d153317ede441
jgrissom <jgrissom91@gmail.com>
2023-04-06 15:57:10 -0500

    [6.09] Create User Login Model
    - Define the model class to support user login
    - UIHint ensures the tag helper will render the field (email, password)

--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -6,3 +6,12 @@ public class CustomerWithPassword
     [UIHint("password"), Required]
     public string Password { get; set; }
 }
+
+public class LoginModel
+{
+    [Required, UIHint("email")]
+    public string Email { get; set; }
+
+    [Required, UIHint("password")]
+    public string Password { get; set; }
+}
